b6c31eed3461a8d1b23e8a149207f4dd44744ab6
Darina Skovysh <darina.skovysh@gmail.com>
2021-07-14 14:49:09 +0300

    updated question controller

--- src/main/java/com/team4/testingsystem/controllers/QuestionController.java
+++ src/main/java/com/team4/testingsystem/controllers/QuestionController.java
@@ -1,28 +1,50 @@
 package com.team4.testingsystem.controllers;
 
-import com.team4.testingsystem.entities.Question;
+import com.team4.testingsystem.converters.QuestionConverter;
+import com.team4.testingsystem.dto.QuestionDTO;
 import com.team4.testingsystem.services.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/question")
 public class QuestionController {
     private final QuestionService questionService;
+    private final QuestionConverter questionConverter;
 
     @Autowired
-    public QuestionController(QuestionService questionService) {
+    public QuestionController(QuestionService questionService, QuestionConverter questionConverter) {
         this.questionService = questionService;
+        this.questionConverter = questionConverter;
     }
 
     @GetMapping("/{id}")
-    public Question getQuestion(@PathVariable("id") Long id) {
-        return questionService.getQuestionById(id);
+    public QuestionDTO getQuestion(@PathVariable("id") Long id) {
+        return questionConverter.convertToDTO(questionService.getQuestionById(id));
     }
 
-    @PostMapping("/update")
-    public Question updateQuestion(@RequestParam Long id, boolean isAvailable) {
-        return questionService.updateAvailability(id, isAvailable);
+    @PostMapping(path = "/add")
+    public QuestionDTO addQuestion(@RequestBody QuestionDTO questionDTO) {
+        return questionConverter.convertToDTO(
+                questionService.createQuestion(questionConverter.convertToEntity(questionDTO))
+        );
     }
 
+    @PostMapping("/delete")
+    public void archiveQuestion(@RequestParam Long id, @RequestParam boolean isAvailable) {
+        questionService.archiveQuestion(isAvailable, id);
+    }
+
+    @PostMapping("/update/{id}")
+    public QuestionDTO updateQuestion(@RequestBody QuestionDTO questionDTO, @PathVariable("id") Long id) {
+        return questionConverter.convertToDTO(
+                questionService.updateQuestion(questionConverter.convertToEntity(questionDTO, id), id)
+        );
+    }
 }
