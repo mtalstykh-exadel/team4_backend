de8896a6dd1b326158ef5883a59f84c7125a98b9
Andrew241 <don.andrew241@gmail.com>
2021-07-21 12:35:23 +0300

    removed 1 unused import

--- src/main/java/com/team4/testingsystem/exceptions/UserNotFoundException.java
+++ src/main/java/com/team4/testingsystem/exceptions/UserNotFoundException.java
@@ -1,6 +1,5 @@
 package com.team4.testingsystem.exceptions;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(reason = "User not found")
