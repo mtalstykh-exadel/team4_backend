a3222d403726f79399c05fd8cca0c35fc4259a43
Max <mkogan58@gmail.com>
2021-07-26 11:42:03 +0300

    Create coach grade entity and repository

---
+++ src/main/java/com/team4/testingsystem/repositories/CoachGradeRepository.java
@@ -0,0 +1,7 @@
+package com.team4.testingsystem.repositories;
+
+import com.team4.testingsystem.entities.CoachGrade;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CoachGradeRepository extends CrudRepository<Long, CoachGrade> {
+}
