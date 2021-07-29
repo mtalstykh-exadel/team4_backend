d5e9fad566510772a9ccfbf1c15d0596995a75c1
Darina Skovysh <darina.skovysh@gmail.com>
2021-07-29 20:55:38 +0300

    added queries for test generating

--- src/main/java/com/team4/testingsystem/repositories/ContentFilesRepository.java
+++ src/main/java/com/team4/testingsystem/repositories/ContentFilesRepository.java
@@ -1,11 +1,14 @@
 package com.team4.testingsystem.repositories;
 
 import com.team4.testingsystem.entities.ContentFile;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public interface ContentFilesRepository extends CrudRepository<ContentFile, Long> {
@@ -19,4 +22,12 @@ public interface ContentFilesRepository extends CrudRepository<ContentFile, Long
     @Modifying
     @Query(value = "DELETE from ContentFile cf WHERE cf.id = ?1")
     int removeById(Long id);
+
+    @Query("SELECT cf from ContentFile cf " +
+           "JOIN cf.questions q " +
+           "JOIN q.level l " +
+           "WHERE l.name = ?1 " +
+           "order by function('random') ")
+    List<ContentFile> getRandomFiles(String level, Pageable pageable);
+
 }
