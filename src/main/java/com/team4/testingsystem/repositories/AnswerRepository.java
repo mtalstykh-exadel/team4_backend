b37830371884b72bbf70128c88578b92246eed52
Semyon <fpm.korostik@bsu.by>
2021-08-02 12:59:56 +0300

    deleted unnecessary files

--- src/main/java/com/team4/testingsystem/repositories/AnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.team4.testingsystem.repositories;
-
-import com.team4.testingsystem.entities.Answer;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AnswerRepository extends CrudRepository<Answer, Long> {
-}
