e4a52f5f772b2249b003a00f513c08859824a90a
Max <mkogan58@gmail.com>
2021-08-03 15:49:23 +0300

    Fix PMD issue and remove SpotBugs filter

--- src/main/java/com/team4/testingsystem/entities/Test.java
+++ src/main/java/com/team4/testingsystem/entities/Test.java
@@ -7,7 +7,6 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
