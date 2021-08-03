f08f132e93e61a44a00edee45ca614df8df4da82
Darina Skovysh <darina.skovysh@gmail.com>
2021-08-03 20:28:39 +0300

    updated endpoint

--- src/main/java/com/team4/testingsystem/controllers/QuestionController.java
+++ src/main/java/com/team4/testingsystem/controllers/QuestionController.java
@@ -2,7 +2,9 @@ package com.team4.testingsystem.controllers;
 
 import com.team4.testingsystem.converters.QuestionConverter;
 import com.team4.testingsystem.dto.QuestionDTO;
+import com.team4.testingsystem.entities.Answer;
 import com.team4.testingsystem.entities.Question;
+import com.team4.testingsystem.repositories.AnswerRepository;
 import com.team4.testingsystem.services.QuestionService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,16 +17,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(path = "/question")
 public class QuestionController {
     private final QuestionService questionService;
     private final QuestionConverter questionConverter;
+    private final AnswerRepository answerRepository;
 
     @Autowired
-    public QuestionController(QuestionService questionService, QuestionConverter questionConverter) {
+    public QuestionController(QuestionService questionService,
+                              QuestionConverter questionConverter,
+                              AnswerRepository answerRepository) {
         this.questionService = questionService;
         this.questionConverter = questionConverter;
+        this.answerRepository = answerRepository;
     }
 
     @ApiOperation(value = "Get a single question from the database by it's id")
@@ -36,7 +44,9 @@ public class QuestionController {
     @ApiOperation(value = "Add a new question")
     @PostMapping("/")
     public QuestionDTO addQuestion(@RequestBody QuestionDTO questionDTO) {
-        Question question = questionService.createQuestion(questionConverter.convertToEntity(questionDTO));
+        Question question = questionService
+                .createQuestion(questionConverter.convertToEntity(questionDTO), questionDTO.getAnswers());
+        answerRepository.updateCorrect(questionDTO.getCorrect());
         return questionConverter.convertToDTO(question);
     }
 
