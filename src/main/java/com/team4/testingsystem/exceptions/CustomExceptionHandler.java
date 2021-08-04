cebe42d3a93ade77b61da08c35fdf0174ddcf199
Max <mkogan58@gmail.com>
2021-08-04 19:08:09 +0300

    Fix SpotBugs issue

--- src/main/java/com/team4/testingsystem/exceptions/CustomExceptionHandler.java
+++ src/main/java/com/team4/testingsystem/exceptions/CustomExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.team4.testingsystem.exceptions;
 
 import org.postgresql.util.PSQLException;
+import org.postgresql.util.ServerErrorMessage;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,8 +15,10 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = {PSQLException.class})
     protected ResponseEntity<Object> handlePostgreSQLException(PSQLException e, WebRequest request) {
         String body = "Incorrect data provided!";
-        if (e.getServerErrorMessage() != null) {
-            body = "Incorrect data provided for column '" + e.getServerErrorMessage().getColumn() + "'";
+
+        final ServerErrorMessage errorMessage = e.getServerErrorMessage();
+        if (errorMessage != null) {
+            body = "Incorrect data provided for column '" + errorMessage.getColumn() + "'";
         }
         return handleExceptionInternal(e, body, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
