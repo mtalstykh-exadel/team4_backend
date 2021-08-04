f83ac65dc00069a1dcaaae343e0422a19906f077
Darina Skovysh <darina.skovysh@gmail.com>
2021-08-04 11:23:40 +0300

    Update TestDTO.java

--- src/main/java/com/team4/testingsystem/dto/TestDTO.java
+++ src/main/java/com/team4/testingsystem/dto/TestDTO.java
@@ -1,11 +1,13 @@
 package com.team4.testingsystem.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.team4.testingsystem.entities.Test;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class TestDTO {
     private String level;
     private LocalDateTime createdAt;
