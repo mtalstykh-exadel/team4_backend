9d981acbf1ffec41bfca971bb11795648790e5c6
Max <mkogan58@gmail.com>
2021-08-04 18:34:26 +0300

    Add exception handler for PSQLException

---
+++ src/main/java/com/team4/testingsystem/exceptions/ExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.team4.testingsystem.exceptions;
+
+import org.postgresql.util.PSQLException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandler extends ResponseEntityExceptionHandler {
+    @org.springframework.web.bind.annotation.ExceptionHandler(value = {PSQLException.class})
+    protected ResponseEntity<Object> handlePostgreSQLException(PSQLException e, WebRequest request) {
+        String body = "Incorrect data provided!";
+        if (e.getServerErrorMessage() != null) {
+            body = "Incorrect data provided for column '" + e.getServerErrorMessage().getColumn() + "'";
+        }
+        return handleExceptionInternal(e, body, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+}
