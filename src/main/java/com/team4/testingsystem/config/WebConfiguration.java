46637b2293060ac8c6fbfc34f273cabd17b45e26
Max <mkogan58@gmail.com>
2021-08-04 12:37:09 +0300

    Update allowed headers

--- src/main/java/com/team4/testingsystem/config/WebConfiguration.java
+++ src/main/java/com/team4/testingsystem/config/WebConfiguration.java
@@ -1,17 +1,20 @@
 package com.team4.testingsystem.config;
 
+import org.apache.http.HttpHeaders;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("*")
-                .allowedHeaders("*");
+                .allowedHeaders(HttpHeaders.AUTHORIZATION, HttpHeaders.CONTENT_TYPE, HttpHeaders.ACCEPT);
     }
 
     @Override
