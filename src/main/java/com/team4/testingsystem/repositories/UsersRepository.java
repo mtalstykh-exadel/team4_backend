8bdf9d40e674406e40361771646356326f877b5d
Max <mkogan58@gmail.com>
2021-08-06 15:24:52 +0300

    Fix import order

--- src/main/java/com/team4/testingsystem/repositories/UsersRepository.java
+++ src/main/java/com/team4/testingsystem/repositories/UsersRepository.java
@@ -6,9 +6,9 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
+import javax.transaction.Transactional;
 
 public interface UsersRepository extends CrudRepository<User, Long> {
     Optional<User> findByLogin(String login);
