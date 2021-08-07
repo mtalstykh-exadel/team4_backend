a0d38ba94ffdcf7fe6cc8acaae4f7b5a1a45c1a2
Andrew241 <don.andrew241@gmail.com>
2021-08-07 20:52:05 +0300

    added entity

---
+++ src/main/java/com/team4/testingsystem/entities/ModuleGrade.java
@@ -0,0 +1,57 @@
+package com.team4.testingsystem.entities;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "module_grade")
+public class ModuleGrade implements Serializable {
+
+    @EmbeddedId
+    TestModuleID id;
+
+    @Column(name = "grade")
+    private Integer grade;
+
+
+    public ModuleGrade() {
+    }
+
+    public TestModuleID getId() {
+        return id;
+    }
+
+    public void setId(TestModuleID id) {
+        this.id = id;
+    }
+
+    public Integer getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Integer grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ModuleGrade that = (ModuleGrade) o;
+        return Objects.equals(id, that.id)
+                && Objects.equals(grade, that.grade);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, grade);
+    }
+}
