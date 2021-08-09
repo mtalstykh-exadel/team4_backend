271fb9ae5331b71ca2bbb08d61896e19c5f2cd43
Andrew241 <don.andrew241@gmail.com>
2021-08-09 16:06:49 +0300

    updated moduleGradeNotFoundException message

--- src/main/java/com/team4/testingsystem/exceptions/ModuleGradeNotFoundException.java
+++ src/main/java/com/team4/testingsystem/exceptions/ModuleGradeNotFoundException.java
@@ -2,6 +2,6 @@ package com.team4.testingsystem.exceptions;
 
 public class ModuleGradeNotFoundException extends NotFoundException {
     public ModuleGradeNotFoundException(String moduleName) {
-        super("Module (" + moduleName + ")grade not found exception");
+        super("Module (" + moduleName + ") grade not found");
     }
 }
