7e8e8c895d3e99070161fdf82935658c151cc2a5
Darina Skovysh <darina.skovysh@gmail.com>
2021-08-11 23:27:42 +0300

    added method to convert date to unix

--- src/main/java/com/team4/testingsystem/config/WebConfiguration.java
+++ src/main/java/com/team4/testingsystem/config/WebConfiguration.java
@@ -1,9 +1,20 @@
 package com.team4.testingsystem.config;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
@@ -15,5 +26,19 @@ public class WebConfiguration implements WebMvcConfigurer {
 
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+        builder.serializerByType(LocalDateTime.class, new JsonSerializer<LocalDateTime>() {
+            @Override
+            public void serialize(LocalDateTime localDateTime,
+                                  JsonGenerator jsonGenerator,
+                                  SerializerProvider serializerProvider) throws IOException {
+                jsonGenerator.writeNumber(localDateTime.toEpochSecond(ZoneOffset.UTC));
+            }
+        });
+        converters.add(new MappingJackson2HttpMessageConverter(builder.build()));
     }
 }
