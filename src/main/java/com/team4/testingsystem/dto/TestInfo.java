c4c7238ecd4a229dfdaf147163328d164f9781d7
Max <mkogan58@gmail.com>
2021-08-11 20:30:36 +0300

    Add test ID to TestInfo

--- src/main/java/com/team4/testingsystem/dto/TestInfo.java
+++ src/main/java/com/team4/testingsystem/dto/TestInfo.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 public class TestInfo implements Serializable {
+    private Long testId;
     private String level;
     private LocalDateTime deadline;
     private String priority;
@@ -15,9 +16,18 @@ public class TestInfo implements Serializable {
     }
 
     public TestInfo(Test test) {
+        this.testId = test.getId();
         this.level = test.getLevel().getName();
         this.deadline = test.getDeadline();
         this.priority = test.getPriority().getName();
+    }
+
+    public Long getTestId() {
+        return testId;
+    }
+
+    public void setTestId(Long testId) {
+        this.testId = testId;
     }
 
     public String getLevel() {
@@ -53,13 +63,14 @@ public class TestInfo implements Serializable {
             return false;
         }
         TestInfo testInfo = (TestInfo) o;
-        return Objects.equals(level, testInfo.level)
+        return Objects.equals(testId, testInfo.testId)
+                && Objects.equals(level, testInfo.level)
                 && Objects.equals(deadline, testInfo.deadline)
                 && Objects.equals(priority, testInfo.priority);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(level, deadline, priority);
+        return Objects.hash(testId, level, deadline, priority);
     }
 }
