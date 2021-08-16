3e00be73f44b1b8bef91a3ec069da8a843bf8cc9
Andrew241 <don.andrew241@gmail.com>
2021-08-17 01:06:47 +0300

    added ability to send archived questions (and listening topics)

--- src/main/java/com/team4/testingsystem/controllers/QuestionController.java
+++ src/main/java/com/team4/testingsystem/controllers/QuestionController.java
@@ -8,6 +8,7 @@ import com.team4.testingsystem.entities.ContentFile;
 import com.team4.testingsystem.entities.Question;
 import com.team4.testingsystem.enums.Levels;
 import com.team4.testingsystem.enums.Modules;
+import com.team4.testingsystem.enums.QuestionStatus;
 import com.team4.testingsystem.services.ContentFilesService;
 import com.team4.testingsystem.services.QuestionService;
 import io.swagger.annotations.ApiOperation;
@@ -55,8 +56,10 @@ public class QuestionController {
     @GetMapping("/")
     @Secured("ROLE_COACH")
     public List<QuestionDTO> getQuestions(@RequestParam("level") Levels level,
-                                          @RequestParam("module") Modules module) {
-        return questionService.getQuestionsByLevelAndModuleName(level, module).stream()
+                                          @RequestParam("module") Modules module,
+                                          @RequestParam("status") QuestionStatus status
+    ) {
+        return questionService.getQuestionsByLevelAndModuleName(level, module, status).stream()
                 .map(QuestionDTO::create)
                 .collect(Collectors.toList());
     }
@@ -82,8 +85,10 @@ public class QuestionController {
 
     @ApiOperation(value = "Get all topics (or get by level)")
     @GetMapping(value = "/listening")
-    public List<ListeningTopicDTO> getListeningTopics(@RequestParam(required = false) Levels level) {
-        return convertToDTO(questionService.getListening(level));
+    public List<ListeningTopicDTO> getListeningTopics(@RequestParam(required = false) Levels level,
+                                                      @RequestParam("status") QuestionStatus status
+                                                      ) {
+        return convertToDTO(questionService.getListening(level, status));
     }
 
     @ApiOperation(value = "Add content file with questions")
--- src/main/java/com/team4/testingsystem/repositories/ContentFilesRepository.java
+++ src/main/java/com/team4/testingsystem/repositories/ContentFilesRepository.java
@@ -23,26 +23,27 @@ public interface ContentFilesRepository extends CrudRepository<ContentFile, Long
     int removeById(Long id);
 
     @Query(value = "select * from language_testing.content_file as cf "
-                   + "join language_testing.question_content_file as qcf on cf.id = qcf.content_file_id "
-                   + "join language_testing.question as q on qcf.question_id = q.id "
-                   + "join language_testing.level as l on l.id = q.level_id "
-                   + "where l.level_name = ?1 and cf.is_available = true "
-                   + "order by RANDOM() limit 1; ", nativeQuery = true)
+        + "join language_testing.question_content_file as qcf on cf.id = qcf.content_file_id "
+        + "join language_testing.question as q on qcf.question_id = q.id "
+        + "join language_testing.level as l on l.id = q.level_id "
+        + "where l.level_name = ?1 and cf.is_available = true "
+        + "order by RANDOM() limit 1; ", nativeQuery = true)
     ContentFile getRandomFiles(String level);
 
     @Query("select cf from ContentFile cf "
-           + "join cf.questions q "
-           + "where q.id = ?1")
+        + "join cf.questions q "
+        + "where q.id = ?1")
     ContentFile getContentFileByQuestionId(Long id);
 
     @Modifying
     @Query(value = "update ContentFile cf set cf.available = false where cf.id = ?1")
     void archiveContentFile(Long id);
 
-    List<ContentFile> findAll();
+    List<ContentFile> findAllByAvailable(boolean isAvailable);
 
     @Query("select distinct cf from ContentFile cf "
-           + "join cf.questions q "
-           + "where q.level.name = ?1 ")
-    List<ContentFile> getContentFiles(String level);
+        + "join cf.questions q "
+        + "where q.level.name = ?1 "
+        + "and cf.available = ?2")
+    List<ContentFile> getContentFiles(String level, boolean isAvailable);
 }
