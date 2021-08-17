595842d33fd5451e0687ff13d0e3f54c95db527b
Darina Skovysh <darina.skovysh@gmail.com>
2021-08-17 19:47:17 +0300

    fix

--- src/main/java/com/team4/testingsystem/repositories/TestsRepository.java
+++ src/main/java/com/team4/testingsystem/repositories/TestsRepository.java
@@ -66,11 +66,6 @@ public interface TestsRepository extends CrudRepository<Test, Long> {
 
     @Transactional
     @Modifying
-    @Query(value = "UPDATE Test t SET t.verifiedAt = ?1 where t.id = ?2")
-    int updateEvaluation(Instant updateDate, Long id);
-
-    @Transactional
-    @Modifying
     @Query(value = "UPDATE Test t SET t.verifiedAt = ?1, t.status = 'VERIFIED' where t.id = ?2")
     int coachSubmit(Instant updateDate, Long id);
 
