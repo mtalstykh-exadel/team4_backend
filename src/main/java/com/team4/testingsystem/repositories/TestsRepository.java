02fba1bd471b37e997fcceb7104f5004807e95fd
Darina Skovysh <darina.skovysh@gmail.com>
2021-08-17 19:26:15 +0300

    added sort and pagination

--- src/main/java/com/team4/testingsystem/repositories/TestsRepository.java
+++ src/main/java/com/team4/testingsystem/repositories/TestsRepository.java
@@ -23,8 +23,13 @@ public interface TestsRepository extends CrudRepository<Test, Long> {
            + "t.verifiedAt desc, t.deadline desc, t.assignedAt desc ")
     List<Test> getAllByUser(User user, Pageable pageable);
 
-    @Query("select t from Test t where t.status in ?1")
-    List<Test> getByStatuses(Status[] statuses);
+    @Query("select t from Test t where t.status in ?1 "
+           + "order by case "
+           + "when t.priority = 'High' then 'A' "
+           + "when t.priority = 'Medium' then 'B' "
+           + "when t.priority = 'Low' then 'C' ELSE t.priority end, "
+           + "t.deadline asc, t.assignedAt desc ")
+    List<Test> getByStatuses(Status[] statuses, Pageable pageable);
 
     @Query("select t from Test t"
            + " where t.user = ?1 "
@@ -32,8 +37,13 @@ public interface TestsRepository extends CrudRepository<Test, Long> {
            + "and t.startedAt >= ?2")
     List<Test> getSelfStartedByUserAfter(User user, Instant date);
 
-    @Query("select t from Test t where t.coach.id = ?1 and t.status in ?2")
-    List<Test> getAllByAssignedCoachAndStatuses(Long coachId, Status[] status);
+    @Query("select t from Test t where t.coach.id = ?1 and t.status in ?2 "
+           + "order by case "
+           + "when t.priority = 'High' then 'A' "
+           + "when t.priority = 'Medium' then 'B' "
+           + "when t.priority = 'Low' then 'C' ELSE t.priority end, "
+           + "t.deadline asc, t.assignedAt desc ")
+    List<Test> getAllByAssignedCoachAndStatuses(Long coachId, Status[] status, Pageable pageable);
 
     @Transactional
     @Modifying
