44157c5bd67ccf644aaf4501f74c4f670906907c
Max <mkogan58@gmail.com>
2021-08-19 15:07:33 +0300

    Fix notifications NullPointerException

--- src/main/java/com/team4/testingsystem/converters/notifications/TestStartedNotificationConverter.java
+++ src/main/java/com/team4/testingsystem/converters/notifications/TestStartedNotificationConverter.java
@@ -3,7 +3,9 @@ package com.team4.testingsystem.converters.notifications;
 import com.team4.testingsystem.dto.NotificationDTO;
 import com.team4.testingsystem.entities.Notification;
 import com.team4.testingsystem.enums.NotificationType;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TestStartedNotificationConverter extends SingleNotificationConverter {
     @Override
     public NotificationDTO convertToDTO(Notification notification) {
