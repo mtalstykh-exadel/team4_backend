07caef995654639a781539b096f845c11b378301
Semyon <fpm.korostik@bsu.by>
2021-08-21 01:26:00 +0300

    restored and fixed controller

---
+++ src/main/java/com/team4/testingsystem/controllers/CoachAnswerController.java
@@ -0,0 +1,26 @@
+package com.team4.testingsystem.controllers;
+
+import com.team4.testingsystem.dto.CoachAnswerDTO;
+import com.team4.testingsystem.services.CoachAnswerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/coach_reports")
+public class CoachAnswerController {
+    private final CoachAnswerService coachAnswerService;
+
+    @Autowired
+    public CoachAnswerController(CoachAnswerService coachAnswerService) {
+        this.coachAnswerService = coachAnswerService;
+    }
+
+    @PutMapping("/")
+    void addAll(List<CoachAnswerDTO> coachAnswers) {
+        coachAnswerService.addAll(coachAnswers);
+    }
+}
