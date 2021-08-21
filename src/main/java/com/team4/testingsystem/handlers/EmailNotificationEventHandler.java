1e9c873839d06c4bcf44656ebc39c059ddf26829
Max <mkogan58@gmail.com>
2021-08-21 16:53:37 +0300

    Fix import order

--- src/main/java/com/team4/testingsystem/handlers/EmailNotificationEventHandler.java
+++ src/main/java/com/team4/testingsystem/handlers/EmailNotificationEventHandler.java
@@ -11,9 +11,9 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
-import java.util.Map;
 
 @Component
 @ConditionalOnProperty(prefix = "email-notifications", name = "enabled", havingValue = "true")
