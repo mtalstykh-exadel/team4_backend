fbbbfa78fe1a29134d3962a8940a100cbc526769
Max <mkogan58@gmail.com>
2021-08-24 21:03:30 +0300

    Reorder imports

--- src/main/java/com/team4/testingsystem/entities/Timer.java
+++ src/main/java/com/team4/testingsystem/entities/Timer.java
@@ -4,6 +4,7 @@ import com.team4.testingsystem.enums.Status;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.Instant;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -14,7 +15,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import java.time.Instant;
 
 @Entity
 @Table(name = "timer")
